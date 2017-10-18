78267edaac036f3262b2814a5b66007268bfc97e
defat <defat@ukr.net>
2017-10-18 15:13:29 +0300

    HTTP requests are made in background thread

--- nameko_zipkin/transport.py
+++ nameko_zipkin/transport.py
@@ -1,12 +1,17 @@
+import logging
+from queue import Queue
+from threading import Thread
 from urllib.request import Request, urlopen
-from abc import abstractmethod
+from abc import abstractmethod, ABCMeta
 
 from nameko.extensions import SharedExtension
 
 from nameko_zipkin.constants import *
 
+logger = logging.getLogger('nameko-zipkin')
 
-class IHandler:
+
+class IHandler(metaclass=ABCMeta):
     def start(self):
         pass
 
@@ -21,11 +26,31 @@ class IHandler:
 class HttpHandler(IHandler):
     def __init__(self, url):
         self.url = url
+        self.queue = Queue()
+        self.thread = None
+
+    def start(self):
+        self.thread = Thread(target=self._poll)
+        self.thread.start()
+
+    def stop(self):
+        self.queue.put(StopIteration)
+        self.thread.join()
 
     def handle(self, encoded_span):
         body = b'\x0c\x00\x00\x00\x01' + encoded_span
         request = Request(self.url, body, {'Content-Type': 'application/x-thrift'}, method='POST')
-        urlopen(request)
+        try:
+            urlopen(request)
+        except:
+            logger.error('Exception handling span', exc_info=True)
+
+    def _poll(self):
+        while True:
+            span = self.queue.get()
+            if span == StopIteration:
+                break
+            self.handle(span)
 
 
 class Transport(SharedExtension):
